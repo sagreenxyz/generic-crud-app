b4490fc28383b51b2a229451c5f3e9cf418218d9
Scott Green <sagreenxyz@gmail.com>
2022-02-28 21:45:03 -0600

    waypoint

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+// DEPENDENCIES
 const express = require('express');
 
 // CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT;
-console.log(PORT);
+// console.log(PORT);
+const app = express();
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to the Generic CRUD Application...');
+});
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Generic CRUD App at http://localhost:${PORT}`);
+});
