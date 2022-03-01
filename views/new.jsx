75891310968fc78ed7d76e8cf84d9cd2cdd2016d
Scott Green <sagreenxyz@gmail.com>
2022-03-01 02:01:55 -0600

    waypoint

--- views/new.jsx
+++ views/new.jsx
@@ -4,7 +4,17 @@ const Default = require('./layouts/Default');
 function New() {
     return (
         <Default>
-            <h2>Add a New Item</h2>`
+            <h2>Add a New Item</h2>
+            <form action="/items" method="POST">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+                <label htmlFor="image">Image</label>
+                <input type="text" name="image" id="image" />
+                <label htmlFor="isItem">Is an Item?</label>
+                <input type="checkbox" name="isItem" id="isItem" defaultChecked />
+                <br />
+                <input type="submit" />
+            </form>
         </Default>
     )
 }
