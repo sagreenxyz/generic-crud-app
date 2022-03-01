ce5544a150b49f7cb595dabfd8e1594fbff998b9
Scott Green <sagreenxyz@gmail.com>
2022-03-01 02:05:51 -0600

    waypoint

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -33,6 +33,9 @@ items.get('/:arrayIndex', (req, res) => {
 
 // CREATE
 items.post('/', (req, res) => {
+    if (!req.body.image) {
+      req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    }
     if(req.body.isItem === 'on') {
         req.body.isItem === 'true';
     } else {
