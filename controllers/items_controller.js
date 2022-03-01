8b3fae181267e86b3ec3939acb2420b4ef36d646
Scott Green <sagreenxyz@gmail.com>
2022-02-28 21:51:15 -0600

    waypoint

--- controllers/items_controller.js
+++ controllers/items_controller.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const items = express.Router();
+const Item = require('../models/items.js')
 
 // INDEX
 items.get('/', (req, res) => {
-    res.send('This is the index at /items');
+    // res.send('This is the index at /items');
+    res.send(Item);
 });
 
 module.exports = items;
