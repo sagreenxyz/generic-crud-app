1f9dff4d70fa2534deed66ef9137b179658aa71b
Scott Green <sagreenxyz@gmail.com>
2022-03-01 02:04:29 -0600

    waypoint

--- views/new.jsx
+++ views/new.jsx
@@ -7,7 +7,7 @@ function New() {
             <h2>Add a New Item</h2>
             <form action="/items" method="POST">
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
+                <input type="text" name="name" id="name" required />
                 <label htmlFor="image">Image</label>
                 <input type="text" name="image" id="image" />
                 <label htmlFor="isItem">Is an Item?</label>
