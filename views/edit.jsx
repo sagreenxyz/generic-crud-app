ab198db53e3d9d068461654239ea247174d2c517
Scott Green <sagreenxyz@gmail.com>
2022-03-01 19:38:00 -0600

    waypoint

--- views/edit.jsx
+++ views/edit.jsx
@@ -1,17 +1,17 @@
 const React = require('react');
 const Default = require('./layouts/Default');
 
-function Edit() {
+function Edit({item, index}) {
     return (
         <Default>
             <h2>Edit an Item</h2>
             <form>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" required />
+                <input type="text" name="name" id="name" required defaultValue={item.name} />
                 <label htmlFor="image">Image</label>
-                <input type="text" name="image" id="image" />
+                <input type="text" name="image" id="image" defaultValue={item.image} />
                 <label htmlFor="isItem">Is Item?</label>
-                <input type="checkbox" name="isItem" id="isItem" defaultCheckec />
+                <input type="checkbox" name="isItem" id="isItem" defaultChecked defaultValue={item.isItem} />
                 <br />
                 <input type="submit" />
             </form>
